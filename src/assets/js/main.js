6773c61cc3d7a38b4223b148c4a82d5b9892f73b
hedlichk <39142518+hedlichk@users.noreply.github.com>
2022-01-26 20:39:34 -0500

    Version 2.0

--- src/assets/js/main.js
+++
@@ -1,8 +0,0 @@
-
-document.getElementById("activeAssessment").addEventListener("click", navAssessment);
-document.getElementById("savedAssessments").addEventListener("click", navSavedAssessments);
-document.getElementById("allAssessments").addEventListener("click", navAllAssessments);
-
-function navAssessment() { window.location.href="/myassessment.html"; };
-function navSavedAssessments() { window.location.href="/mylisting.html"; };
-function navAllAssessments() { window.location.href="/allassessments.html"; };
